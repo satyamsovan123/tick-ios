553497a2fad1dfe2a90d17da9d6d656f5cfdb40a
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2023-09-08 00:18:20 +0530

    Initial Commit

---
+++ Tick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tick
+//
+//  Created by Satyam Sovan Mishra on 08/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
